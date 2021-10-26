61a993795acd873ed020cd33d25947b7471dee2b
Albert Kjøller Jacobsen <albert_k_jacobsen@hotmail.com>
2021-10-26 17:44:42 +0100

    small changes

--- content/basics.py
+++ content/basics.py
@@ -13,50 +13,49 @@ from tqdm import tqdm
 from vizdoom import vizdoom
 import numpy as np
 import itertools as it
+from multiprocessing import cpu_count, Process
 import matplotlib.pyplot as plt
 
 
-class Environment():
+class Environment:
     """
     Environment setup - using VizDoom variable names for the game environment to create an OpenAI-like instance
     for consistency with OpenAI gym tools
     """
 
-    def __init__(self, config_file_path, window_visible=False, depth=False, labels=False):
+    def __init__(self, config_file_path, players, window_visible=False, depth=False):
 
-        self.initialize_vizdoom(config_file_path, window_visible, depth, labels)
+        self.config_file_path = config_file_path
+        self.window_visible = window_visible
+        self.depth = depth
+        self.players = players
+
+        self.initialize_player()
         n = self.game.get_available_buttons_size()
         self.actions = [list(a) for a in it.product([0, 1], repeat=n)]
         self.actions_size = len(self.actions)
 
-    def initialize_vizdoom(self, config_file_path, window_visible=False, depth=False, labels=False):
+    def initialize_player(self):
         """
         Load configuration from path...
         """
-
         print("Initializing doom...")
         self.game = vizdoom.DoomGame()
-        self.game.load_config(config_file_path)
-        self.game.set_window_visible(window_visible)
-        self.game.set_mode(vizdoom.Mode.PLAYER)
-        #self.game.set_screen_format(vizdoom.ScreenFormat.GRAY8)
+        self.game.load_config(self.config_file_path)
+        self.game.set_window_visible(self.window_visible)
+        self.game.set_mode(vizdoom.Mode.PLAYER) #TODO: CHECK MODE Async or not
         self.game.set_screen_format(vizdoom.ScreenFormat.RGB24)
         self.game.set_screen_resolution(vizdoom.ScreenResolution.RES_640X480)
-        self.game.set_depth_buffer_enabled(depth)
-        self.game.set_labels_buffer_enabled(labels)
+        self.game.set_depth_buffer_enabled(self.depth)
         print("Doom setup succesfull.")
 
     def step(self, a):
         """
         Step function for interaction in the environment
         """
-
         r = self.game.make_action(a)
         done = self.game.is_episode_finished()
-
-        # TODO: Make it more smooth - in case we want to use depth and label buffers.
-        # TODO: Also update of state when done could be done smarter
-        sp = self.game.get_state().screen_buffer if not done else 0
+        sp = self.game.get_state().screen_buffer if not done else 0 # TODO: could be done smarter, state could include depth
         return sp, r, done
 
 
@@ -81,10 +80,12 @@ class Agent:
         pass
 
 
-def train(env, agent, episodes):
+def train(env, agent, episodes, episode_length=2100):
     """General training loop - applicable to multiple different agents"""
 
+    env.game.set_episode_timeout(episode_length)
     env.game.init()
+
     for episode_iteration in tqdm(range(episodes)):
         env.game.new_episode()
 
@@ -93,16 +94,11 @@ def train(env, agent, episodes):
 
         with tqdm(total=env.game.get_episode_timeout(), desc=f"Episode {episode_iteration}", position=0, leave=True, colour='green') as tq:
             while not env.game.is_episode_finished():
-                # TODO: Determine notation of states, actions, etc. (OpenAI gym or VizDoom examples?)
-                # TODO: As well as which features to use
                 s = env.game.get_state()
                 screen = s.screen_buffer
                 depth = s.depth_buffer
-                segment = s.labels_buffer
-                automap = s.automap_buffer
-                #labels = s.labels
 
-                a = agent.pi(s=screen)
+                a = agent.pi(s=(screen, depth))
                 sp, r, done = env.step(a)
                 agent.train(s, a, r, sp, done)
 
@@ -111,17 +107,23 @@ def train(env, agent, episodes):
                 tq.update()
 
 
-            #TODO: Depending on the policy, we might want to save the trajectories?
-
-
-
 if __name__ == '__main__':
 
     config_file_path = "setting/settings.cfg"
+    cameras = 8  # number of players
 
-    env = Environment(config_file_path=config_file_path, window_visible=True, depth=True, labels=True)
+    env = Environment(config_file_path=config_file_path, window_visible=True, depth=True, players=cameras)
     agent = Agent(env)
 
     num_episodes = 10
-    train(env, agent, episodes=num_episodes)
+    episode_length = 2100
 
+    train(env, agent, episodes=num_episodes, episode_length=episode_length)
+
+"""
+    processes = []
+    for i in range(1, cameras):
+        p_join = Process(target=player_join, args=(i,))
+        p_join.start()
+        processes.append(p_join)
+"""
